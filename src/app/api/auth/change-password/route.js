7cb8dc3a55df6d15ff99f5b2ad83ea583e955d84
szymekst <Denek222@gmail.com>
2025-06-26 20:57:48 +0200

    [ADD] change-password api with backend validation

---
+++ src/app/api/auth/change-password/route.js
@@ -0,0 +1,69 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import bcrypt from "bcryptjs";
+import User from "@/models/User";
+import Token from "@/models/Token";
+import { NextResponse } from "next/server";
+import { ChangePasswordSchema } from "@/utils/zodSchemas";
+
+export async function POST(req) {
+    try {
+        const { token, password, confirmPassword } = await req.json();
+
+        if (!token) {
+            return NextResponse.json(
+                { message: "Missing token" },
+                { status: 400 }
+            );
+        }
+
+        if (!password) {
+            return NextResponse.json(
+                { message: "Missing password" },
+                { status: 400 }
+            );
+        }
+
+        const parsed = ChangePasswordSchema.safeParse({
+            password,
+            confirmPassword,
+        });
+        if (!parsed.success) {
+            return NextResponse.json(
+                { message: "Data validation failed" },
+                { status: 400 }
+            );
+        }
+
+        await connectMongoDB();
+
+        const foundToken = await Token.findOne({
+            token,
+            type: "passwordReset",
+        });
+
+        if (!foundToken) {
+            return NextResponse.json(
+                { message: "Invalid or expired token" },
+                { status: 400 }
+            );
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+        await User.findByIdAndUpdate(foundToken.userId, {
+            password: hashedPassword,
+        });
+
+        await Token.deleteOne({
+            _id: foundToken._id,
+            type: "passwordReset",
+        });
+
+        return NextResponse.json(
+            { message: "Password has been changed, you can log in" },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message: "Server error" }, { status: 500 });
+    }
+}
