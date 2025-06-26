3ccc198e415f42064665a486b4d481e8102b372c
szymekst <Denek222@gmail.com>
2025-06-26 22:14:20 +0200

    [CHANGE] Verify email form

--- src/app/(auth)/verify-email/page.jsx
+++ src/app/(auth)/verify-email/page.jsx
@@ -1,51 +1,13 @@
-"use client";
-import React, { useState } from "react";
-import { useSearchParams, useRouter } from "next/navigation";
+import React from "react";
+import VerifyEmailForm from "@/components/authComponents/VerifyEmailForm";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-const VerifyEmail = () => {
-    const searchParams = useSearchParams();
-    const token = searchParams.get("token");
-    const router = useRouter();
-    const [status, setStatus] = useState("idle");
-
-    const handleVerify = async () => {
-        const res = await fetch("/api/verify", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ token }),
-        });
-
-        if (res.ok) {
-            setStatus("success");
-            setTimeout(() => router.push("/"), 2000);
-        } else {
-            setStatus("error");
-        }
-    };
-
-    return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
-            <h1 className="text-2xl font-semibold mb-4">
-                Potwierdź swoje konto
-            </h1>
-            <button
-                onClick={handleVerify}
-                className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition"
-            >
-                Potwierdź konto
-            </button>
-            {status === "success" && (
-                <p className="text-green-600 mt-4">
-                    Konto aktywne! Przekierowanie...
-                </p>
-            )}
-            {status === "error" && (
-                <p className="text-red-600 mt-4">
-                    Błąd tokenu lub konto już aktywne.
-                </p>
-            )}
-        </div>
-    );
+const VerifyEmail = async () => {
+    const session = await getServerSession(authOptions);
+    if (session) redirect("/dashboard");
+    return <VerifyEmailForm />;
 };
 
 export default VerifyEmail;
