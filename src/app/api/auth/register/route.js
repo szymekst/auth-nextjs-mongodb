1ab66eb9944c1624451325f8e3c622d1e226669d
szymekst <Denek222@gmail.com>
2025-06-26 20:53:04 +0200

    [CHANGE] Register API, add backend validation

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -3,52 +3,83 @@ import crypto from "crypto";
 import { Resend } from "resend";
 import { connectMongoDB } from "@/src/lib/mongodb";
 import { NextResponse } from "next/server";
+
 import User from "@/src/models/users";
+import Token from "@/models/Token";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req) {
     try {
-        const { name, email, password } = await req.json();
-        const hashedPassword = await bcrypt.hash(password, 12);
-        const token = crypto.randomBytes(32).toString("hex");
+        const body = await req.json();
+
+        const parsed = RegisterSchema.safeParse(body);
+        if (!parsed.success) {
+            const fieldErrors = parsed.error.flatten().fieldErrors;
+
+            return NextResponse.json(
+                { message: fieldErrors || "Something went wrong!" },
+                { status: 400 }
+            );
+        }
 
         await connectMongoDB();
 
+        const { name, email, password } = parsed.data;
         const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return NextResponse.json(
-                { message: "Email already in use." },
-                { status: 409 } // conflict status
-            );
-        } else {
-            await User.create({
-                name,
-                email,
-                password: hashedPassword,
-                emailVerificationToken: token,
-            });
 
-            await resend.emails.send({
-                from: "Pettie <noreply@pettie.pl>",
-                to: email,
-                subject: "Pettie - Potwierdź swoje konto!",
-                html: `
+        if (existingUser) {
+            if (!existingUser.isActive) {
+                await Token.deleteOne({
+                    userId: existingUser._id,
+                    type: "emailVerification",
+                });
+                await User.deleteMany({ _id: existingUser._id });
+            } else {
+                return NextResponse.json(
+                    { message: "Email already in use." },
+                    { status: 409 }
+                );
+            }
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const newUser = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+            isActive: false,
+        });
+
+        const token = crypto.randomBytes(32).toString("hex");
+
+        await Token.create({
+            userId: newUser._id,
+            email: newUser.email,
+            token,
+            type: "emailVerification",
+        });
+
+        await resend.emails.send({
+            from: "Pettie <noreply@pettie.pl>",
+            to: email,
+            subject: "Pettie - Potwierdź swoje konto!",
+            html: `
                     <p>Kliknij poniższy link, aby aktywować konto:</p>
                     <a href="${process.env.NEXTAUTH_URL}/verify?token=${token}">
                     <button style="padding:10px 20px; background:#0070f3; color:white;">Potwierdź konto</button>
                     </a>
                     `,
-            });
+        });
 
-            return NextResponse.json(
-                { message: "User registered. Verify account to LogIn." },
-                { status: 201 }
-            );
-        }
-    } catch (error) {
         return NextResponse.json(
-            { message: "An error occurred while registering the user." },
+            { message: "User registered. Verify account to Log in." },
+            { status: 201 }
+        );
+    } catch (error) {
+        console.error("REGISTER ERROR:", error);
+        return NextResponse.json(
+            { message: "Unable to connect to the database!" },
             { status: 500 }
         );
     }
