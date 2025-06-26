ea837481a451384ff7f839dc7fb00ad6062193be
szymekst <Denek222@gmail.com>
2025-06-26 21:10:43 +0200

    [CHANGE] Folder structure for pages

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import RegisterForm from "../../components/RegisterForm";
+import RegisterForm from "../../../components/RegisterForm";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../../api/auth/[...nextauth]/route";
 
 const Register = async () => {
     const session = await getServerSession(authOptions);
