3d2e1515c6846e931b52e828ed36f1c1ffde74a7
szymekst <Denek222@gmail.com>
2025-06-29 02:11:25 +0200

    [CHANGE] lib mongodb to TS

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -2,9 +2,12 @@ import mongoose from "mongoose";
 
 export const connectMongoDB = async () => {
     try {
+        if (!process.env.MONGODB_URI) {
+            throw new Error("MONGODB_URI is not defined");
+        }
         await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
-        console.log("Error connecting to MongoDB: ", error);
+        console.error("Error connecting to MongoDB: ", error);
     }
 };
