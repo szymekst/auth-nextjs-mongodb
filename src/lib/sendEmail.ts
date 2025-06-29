944cc1d1f54a0395f7f0c53897e8a8af48cb3693
szymekst <Denek222@gmail.com>
2025-06-29 02:30:28 +0200

    [CHANGE] sendEmail to TS

--- src/lib/sendEmail.ts
+++ src/lib/sendEmail.ts
@@ -1,9 +1,15 @@
 import nodemailer from "nodemailer";
 import { render } from "@react-email/components";
 
+type sendEmailProps = {
+    to: string;
+    subject: string;
+    emailToHtml: React.ReactElement;
+};
+
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
+    host: process.env.SMTP_HOST || "",
+    port: Number(process.env.SMTP_PORT),
     secure: false,
     auth: {
         user: process.env.SMTP_USER,
@@ -11,7 +17,11 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-export const sendEmail = async ({ to, subject, emailToHtml }) => {
+export const sendEmail = async ({
+    to,
+    subject,
+    emailToHtml,
+}: sendEmailProps) => {
     const html = await render(emailToHtml);
     return transporter.sendMail({
         from: `${process.env.EMAIL_FROM} <noreply@${process.env.YOUR_DOMAIN}>`,
